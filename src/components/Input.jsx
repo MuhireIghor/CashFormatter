a5a8fe17225a49dd18ed2e10f73dde1b2013432e
MuhireIghor <muhireighor123@gmail.com>
2023-04-26 15:23:11 +0200

    [refactor]:Fixed the inputted data validation

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,19 +2,26 @@ import { useState } from "react";
 import { formatCurrency } from "../utils";
 
 const Inputs = () => {
-    const [value, setValue] = useState(0)
-
-
-
+    const [inputVal, setInputVal] = useState(0);
+    const [value, setValue] = useState(0);
+    const [isDisabled,setIsDisabled] = useState(false);
     const handleFormat = () => {
         const nums = formatCurrency(inputVal);
         setValue(nums)
     }
+    const handleInput = (e) => {
+        const { value } = e.target;
+        const numDots = value.split(".");
+        if (numDots.length-1 >1) {
+            setIsDisabled(true);
+            return;
+        }
+        setInputVal(value);
+    }
 
-    const [inputVal, setInputVal] = useState(0);
     return (
         <div className="bg-white rounded ">
-            <input type="text" value={inputVal} onChange={(e) => setInputVal(e.target.value)} className="bg-white w-2/3 border" />
+            <input type="text" value={inputVal} onChange={handleInput} className="bg-white w-2/3 border" disabled={isDisabled} />
             <button onClick={handleFormat}>Format</button>
             <p>{value}</p>
 
